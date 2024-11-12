acbfa2828eaa6d92841cdf49a45db02527056935
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2024-11-12 19:37:13 +0200

    Add mock server response and display response data

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 import { ref } from 'vue'
 
 const loading = ref(false)
+const schedule = ref({})
 
 async function fetchData() {
     try {
         loading.value = true;
-        await fetch('/api/schedule')
+        const response = await fetch('/api/schedule')
+        const json = await response.json()
+        schedule.value = json
     } finally {
         loading.value = false
     }
@@ -15,8 +18,17 @@ async function fetchData() {
 
 <template>
     <div class="schedule">
-        <div v-if="loading">. . .</div>
         <button @click="fetchData">Fetch</button>
+        <div v-if="schedule" class="schedule">
+            <div v-for="(session, key) in schedule.sessions" :key="key" class="session">
+                <div v-for="(thesis, key) in session.thesis" :key="key">
+                    <span class="author">{{ thesis.firstName }} {{ thesis.lastName }}</span>
+                    <span class="title">{{ thesis.title }}</span>
+                    <span class="supervisor">{{ thesis.supervisor.firstName }} {{ thesis.supervisor.lastName }}</span>
+                    <span class="reviewer">{{ thesis.reviewer.firstName }} {{ thesis.reviewer.lastName }}</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,41 @@
-import { http } from 'msw'
+import { http, HttpResponse } from 'msw'
 
 export const handlers = [
     http.get('/api/schedule', () => {
-        console.log('Captured a "GET /api/schedule" request successfully!')
+        return HttpResponse.json({
+            id: 0,
+            sessions: [
+                {
+                    thesis: [
+                        {
+                            firstName: 'Bob',
+                            lastName: 'Smith',
+                            title: 'Software engineering future with AI',
+                            supervisor: {
+                                firstName: 'Jhon',
+                                lastName: 'Trump',
+                            },
+                            reviewer: {
+                                firstName: 'Lo',
+                                lastName: 'Ballz',
+                            }
+                        },
+                        {
+                            firstName: 'Connor',
+                            lastName: 'McArthur',
+                            title: 'Project management tools',
+                            supervisor: {
+                                firstName: 'Simbal',
+                                lastName: 'Rivers',
+                            },
+                            reviewer: {
+                                firstName: 'Lo',
+                                lastName: 'Ballz',
+                            }
+                        }
+                    ]
+                },
+            ],
+        })
     }),
 ]
