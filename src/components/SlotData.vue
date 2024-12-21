f11c7f31dbcacbfb308ff383226a9f5fde109858
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2024-12-21 17:55:38 +0200

    Improve indictments popup styles

--- src/components/SlotData.vue
+++ src/components/SlotData.vue
@@ -6,15 +6,18 @@
         @mouseleave="showIndictment = false"
     >
         <span v-if="hasIndictment" v-show="showIndictment" class="indictment-popup">
-            <p v-for="constraint of constraints" :key="constraint.name" class="indictment-text">{{ constraint.name }}</p>
+            <p>Indictments</p>
+            <ul>
+                <li v-for="constraint of constraints" :key="constraint.name" class="indictment-text">{{ constraint.name }}</li>
+            </ul>
         </span>
         {{ object.name }}
     </td>
 </template>
 
 <script setup lang="ts">
-import { type Person, type Thesis } from '@/types/app';
-import { ref } from 'vue';
+import { type Person, type Thesis, type ConstraintMatch } from '@/types/app';
+import { ref, onUpdated } from 'vue';
 
 const props = defineProps<{
     object: Person | Thesis
@@ -23,18 +26,46 @@ const props = defineProps<{
 
 const showIndictment = ref<boolean>(false)
 
-const constraints = props.object.indictments.map(indictment => {
-    return indictment.constraintMatches
-})[0]
+let constraints: ConstraintMatch[] = []
+
+onUpdated(() => {
+    constraints = props.object.indictments.map(indictment => {
+        return indictment.constraintMatches
+    })[0]
+})
 </script>
 
 <style scoped>
 .indictment-popup {
     background-color: white;
-    padding: 20px;
+    padding: 15px;
     position: absolute;
     right: 0;
-    transform: translateY(-125%);
+    transform: translateY(calc(-105% - 10px));
+    width: 400px;
+    box-sizing: border-box;
+    border: 1px solid var(--gray);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+}
+
+.indictment-popup ul {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+}
+
+.indictment-popup p {
+    font-weight: bold;
+    margin-top: 0;
+}
+
+.indictment-text {
+    margin-bottom: 5px;
+}
+
+.indictment-text:last-child {
+    margin-bottom: 0;
 }
 
 .indictment {
