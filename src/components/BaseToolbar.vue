4a854f5af1242b7d911def38ff6c700774ccc024
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2024-12-23 13:11:45 +0200

    Refactor ToolbarComponent into base and specific versions

---
+++ src/components/BaseToolbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="toolbar">
+        <div class="left">
+            <slot name="left"></slot>
+        </div>
+        <div class="middle">
+            <slot name="middle"></slot>
+        </div>
+        <div class="right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.toolbar {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.left, .middle, .right {
+    display: flex;
+    align-items: center;
+}
+
+.right {
+    gap: 5px;
+}
+</style>
---
+++ src/components/SolutionToolbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <BaseToolbar>
+        <template #left>
+            <div v-if="scoreLoaded" class="score">
+                Score: <strong>{{ props.score }}</strong>
+            </div>
+        </template>
+        <template #middle>
+            <SolutionTitle />
+        </template>
+        <template #right>
+            <BaseButton @click="solveSolution" color="green">Solve</BaseButton>
+            <BaseButton @click="loadSolutionApi" color="gray">Load</BaseButton>
+            <BaseButton @click="loadIndictments" color="gray">Indictments</BaseButton>
+            <BaseButton @click="printSolution" color="gray">Print</BaseButton>
+            <BaseButton @click="printSolvePayload" color="gray">Payload</BaseButton>
+        </template>
+    </BaseToolbar>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import BaseButton from '@/components/BaseButton.vue';
+import SolutionTitle from '@/components/SolutionTitle.vue';
+import useSolutionState from '@/composables/useSolutionState';
+import BaseToolbar from './BaseToolbar.vue';
+
+const props = defineProps<{
+    score: string
+}>()
+
+const { solveSolution, loadSolutionApi, loadIndictments, printSolution, printSolvePayload } = useSolutionState()
+
+const scoreLoaded = computed(() => {
+    return props.score !== ""
+})
+</script>
