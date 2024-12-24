cc1dce9cc421c81637d4b01e97c823943d8aa001
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2024-12-25 00:49:14 +0200

    Add option to import persons via JSON

--- src/components/PersonForm.vue
+++ src/components/PersonForm.vue
@@ -5,36 +5,46 @@
                 <template #left></template>
                 <template #middle></template>
                 <template #right>
-                    <BaseButton v-if="!showForm" @click="showForm = !showForm" color="green">Add Person</BaseButton>
+                    <BaseButton v-if="!showJson && !showForm" @click="showJson = !showJson" color="green">Import JSON</BaseButton>
+                    <BaseButton v-if="!showForm && !showJson" @click="showForm = !showForm" color="green">Add Person</BaseButton>
                     <div v-if="showForm" class="form-buttons">
                         <BaseButton @click="handleSubmit" color="green">Save</BaseButton>
                         <BaseButton @click="showForm = !showForm" color="red">Close</BaseButton>
                     </div>
+                    <div v-if="showJson" class="form-buttons">
+                        <BaseButton @click="handleSubmitJson" color="green">Save</BaseButton>
+                        <BaseButton @click="showJson = !showJson" color="red">Close</BaseButton>
+                    </div>
                 </template>
             </BaseToolbar>
-            <div v-if="!showForm" class="person-list">
-                <div v-if="hasPersons">
-                    <PersonFormSlot v-for="person of persons" :person="person" :key="person.id" @edit-person="handleEditPerson" />
+            <div v-if="!showForm && !showJson" class="person-list-wrapper">
+                <div v-if="hasPersons" class="person-list">
+                    <PersonFormSlot v-for="person of persons" :person="person" :key="person.id"
+                        @edit-person="handleEditPerson" />
                 </div>
                 <div v-else class="alert">
                     <p>Add a person to the list</p>
                 </div>
             </div>
-            <div v-else class="person-form">
+            <div v-else-if="showForm" class="person-form">
                 <form @submit.prevent="handleSubmit">
                     <div class="field">
                         <label for="name">Name</label>
                         <input id="name" name="name" v-model="person.name" />
                     </div>
                     <div class="field">
-                        <TimeConstraintForm
-                            :time-constraints="person.timeConstraints"
+                        <TimeConstraintForm :time-constraints="person.timeConstraints"
                             @save-time-constraint="handleSaveTimeConstraint"
-                            @delete-time-constraint="handleDeleteTimeConstraint"
-                        />
+                            @delete-time-constraint="handleDeleteTimeConstraint" />
                     </div>
                 </form>
                 <BaseButton v-if="isSavedPerson" @click="handleDeletePerson" color="red">Delete</BaseButton>
+            </div>
+            <div v-else-if="showJson" class="json-editor-container">
+                <form @submit.prevent="handleSubmitJson">
+                        <json-editor class="json-editor" mode="tree" :main-menu-bar="false" :navigation-bar="true"
+                            v-model="personsJson" />
+                </form>
             </div>
         </div>
     </ModalComponent>
@@ -48,12 +58,23 @@ import ModalComponent from '@/components/ModalComponent.vue';
 import PersonFormSlot from '@/components/PersonFormSlot.vue';
 import usePersonState from '@/composables/usePersonState'
 import TimeConstraintForm from '@/components/TimeConstraintForm.vue';
+import JsonEditor from 'vue3-ts-jsoneditor'
 import { computed, ref } from 'vue'
 
 const { persons, savePerson, deletePerson, saveTimeConstraint, deleteTimeConstraint } = usePersonState()
 const hasPersons = computed(() => persons.value.length > 0)
 const isSavedPerson = computed(() => person.value.id !== null)
 const showForm = ref(false)
+const showJson = ref(false)
+
+const personsJson = ref<Person[]>([
+    {
+        id: null,
+        name: 'Example Person',
+        timeConstraints: [],
+        indictments: [],
+    }
+])
 const person = ref<Person>({
     id: null,
     name: '',
@@ -75,6 +96,13 @@ const handleSubmit = () => {
     savePerson(person.value)
     resetPerson()
     showForm.value = false
+}
+
+const handleSubmitJson = () => {
+    console.log("Saving persons", personsJson.value)
+    personsJson.value.forEach(person => savePerson(person))
+    personsJson.value = []
+    showJson.value = false
 }
 
 const handleDeletePerson = () => {
@@ -104,6 +132,7 @@ const handleEditPerson = (personToEdit: Person) => {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    height: 100%;
 }
 
 .form-buttons {
@@ -111,13 +140,31 @@ const handleEditPerson = (personToEdit: Person) => {
     gap: 5px;
 }
 
+.person-list-wrapper {
+    gap: 10px;
+}
+
 .person-list {
-    text-align: center;
+    display: flex;
+    gap: 5px;
 }
 
 .person-form {
     display: flex;
     flex-direction: column;
     gap: 10px;
+}
+
+.json-editor-container {
+  flex-grow: 1;
+  overflow: auto;
+}
+
+.json-editor-container form {
+    height: 100%;
+}
+
+.json-editor {
+  height: 100%;
 }
 </style>
